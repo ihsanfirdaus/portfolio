01930cb53208f13d744b712ca83cac7a74df15f8
ihsanfirdaus <ihsanfirdaus0221@gmail.com>
2023-06-01 12:40:42 +0700

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      A full stack developer is a professional who possesses knowledge and expertise in both front-end and back-end web development. This comprehensive skill set enables them to work on various aspects of web development, including server-side programming, client-side programming, and database management
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
